51d382576a5e8a0a8d3fafb8c8fd850fb8f17ebf
Eugene Doe <megistion@gmail.com>
2015-08-16 05:10:43 +0300

    inception

---
+++ src/main/java/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.inject.Inject;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class DemoApplication {
+    @Inject
+    String foo;
+    @Inject
+    String bar;
+
+    public static void main(String[] args) {
+        DemoApplication app = run(new Object[]{DemoApplication.class, "config.groovy"}, args).getBean(DemoApplication.class);
+        System.out.print(app.sayFooBar());
+    }
+
+    private String sayFooBar() {
+        return foo + " " + bar;
+    }
+}
---
+++ src/main/resources/config.groovy
@@ -0,0 +1,8 @@
+beans {
+    xmlns ctx:"http://www.springframework.org/schema/context"
+    ctx."component-scan"("base-package": "demo")
+    ctx.'annotation-config'()
+
+    foo String, "hello"
+    bar String, "world"
+}
